33eb2ba7a4a5da1761ee02d9192014ba61b17871
Jeremy Dickey <47309416+JDEE30@users.noreply.github.com>
2021-05-25 19:42:35 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./App.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from 'react-router-dom';
 
